16807d9b72374a61d93e2b43be21c38836c9bcf9
Praveen Kumar <pdnsjce@gmail.com>
2018-08-25 23:07:28 +0200

    setup webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  }
+};
