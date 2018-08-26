033c97e5926185b794363207baea3b4b1378eff7
Praveen Kumar <pdnsjce@gmail.com>
2018-08-26 20:26:22 +0200

    global styles to fix border box

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { injectGlobal } from 'emotion';
 
 import Home from './home';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
+
+injectGlobal`
+  * {
+    box-sizing: border-box;
+  }
+`;
