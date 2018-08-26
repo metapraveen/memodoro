7df31e5f3deec2324c2f7e125a7b4373dccca8e4
Praveen Kumar <pdnsjce@gmail.com>
2018-08-26 20:27:32 +0200

    ability to start, discard and validate the task

--- src/home.js
+++ src/home.js
@@ -1,19 +1,71 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import { css } from 'emotion';
 
 class Home extends React.Component {
   state = {
+    isActive: false,
     task: '',
-    time: 20
+    time: 20,
+    errors: {
+      task: false,
+      time: false
+    }
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <input value={this.state.task} onChange={this.handleTaskChange} />
-        <input value={this.state.time} onChange={this.handleTimeChange} />
-        <div>start</div>
-      </React.Fragment>
-    );
+    const { isActive, task, time, countdownTimer, errors } = this.state;
+
+    if (isActive) {
+      return (
+        <div className={containerClass}>
+          <div className={flexContainer}>
+            <div className="task">
+              <div className="label">current task </div>
+              <div className="readonly-task">{task}</div>
+            </div>
+
+            <div className="time">
+              <div className="label"> time left </div>
+              <div className="readonly-task">{countdownTimer}</div>
+            </div>
+
+            <button className="discard" onClick={this.discardCurrentTask}>
+              discard
+            </button>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className={containerClass}>
+          <div className={flexContainer}>
+            <div className="task">
+              <div className="label"> your next task </div>
+              <input
+                required
+                value={task}
+                onChange={this.handleTaskChange}
+                className={classNames({ required: errors.task })}
+              />
+            </div>
+
+            <div className="time">
+              <div className="label"> time </div>
+              <input
+                type="number"
+                required
+                value={time}
+                onChange={this.handleTimeChange}
+                className={classNames({ required: errors.time })}
+              />
+            </div>
+
+            <button onClick={this.startPromodoro}>start</button>
+          </div>
+        </div>
+      );
+    }
   }
 
   handleTaskChange = event => {
@@ -23,6 +75,107 @@ class Home extends React.Component {
   handleTimeChange = event => {
     this.setState({ time: event.target.value });
   };
+
+  startPromodoro = () => {
+    const { task, time } = this.state;
+
+    if (!task || !time) {
+      this.setState({
+        errors: {
+          task: !task,
+          time: !time
+        }
+      });
+
+      return;
+    } else {
+      this.setState({
+        errors: {
+          task: false,
+          time: false
+        }
+      });
+    }
+
+    this.setState({
+      isActive: true,
+      countdownTimer: time
+    });
+
+    // keep decementing time by minutes once task starts
+    setInterval(
+      () =>
+        this.setState({
+          countdownTimer: this.state.countdownTimer - 1
+        }),
+      1000 * 60
+    );
+  };
+
+  discardCurrentTask = () => {
+    this.setState({
+      isActive: false,
+      task: '',
+      time: 20
+    });
+  };
 }
 
 export default Home;
+
+const containerClass = css`
+  margin: 0 auto;
+  width: 50%;
+  margin-top: 5%;
+`;
+
+const flexContainer = css`
+  display: flex;
+  justify-content: space-around;
+
+  input,
+  .readonly-task {
+    width: 100%;
+    height: 35px;
+    border-radious: 5%;
+    font-family: monospace;
+    padding: 5px;
+    font-size: 14px;
+
+    &.required {
+      border: 1px solid red;
+    }
+  }
+
+  .task {
+    width: 60%;
+  }
+
+  .time {
+    width: 10%;
+
+    input {
+      text-align: center;
+    }
+  }
+
+  button {
+    height: 35px;
+    font-size: 15px;
+    font-weight: 600;
+    background-color: green;
+    align-self: flex-end;
+
+    &.discard {
+      background-color: red;
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .label {
+    padding: 2px;
+  }
+`;
